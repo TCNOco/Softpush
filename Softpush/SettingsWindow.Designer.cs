40f9756d12ae53e4f248bb33c5c5dbdd8642faa0
Wesley Pyburn (TechNobo) <technobo@tcno.co>
2019-10-23 02:23:11 +0200

    Init
    Init. Some basic functionality in place.

---
+++ Softpush/SettingsWindow.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Softpush
+{
+    partial class frmSoftpushOptions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBlockedURLs = new System.Windows.Forms.Label();
+            this.btnSaveURL = new System.Windows.Forms.Button();
+            this.txtURLs = new System.Windows.Forms.TextBox();
+            this.lblURLFormat = new System.Windows.Forms.Label();
+            this.gbOptions = new System.Windows.Forms.GroupBox();
+            this.gbKeepAlive = new System.Windows.Forms.GroupBox();
+            this.btnKeepAliveUninstall = new System.Windows.Forms.Button();
+            this.btnKeepAliveInstall = new System.Windows.Forms.Button();
+            this.lblKeepAliveInfo = new System.Windows.Forms.Label();
+            this.statusStripMain = new System.Windows.Forms.StatusStrip();
+            this.tsslblWebServer = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsslblSpacer = new System.Windows.Forms.ToolStripStatusLabel();
+            this.gbOptions.SuspendLayout();
+            this.gbKeepAlive.SuspendLayout();
+            this.statusStripMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblBlockedURLs
+            // 
+            this.lblBlockedURLs.AutoSize = true;
+            this.lblBlockedURLs.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBlockedURLs.Location = new System.Drawing.Point(12, 9);
+            this.lblBlockedURLs.Name = "lblBlockedURLs";
+            this.lblBlockedURLs.Size = new System.Drawing.Size(92, 13);
+            this.lblBlockedURLs.TabIndex = 2;
+            this.lblBlockedURLs.Text = "Blocked URLs:";
+            // 
+            // btnSaveURL
+            // 
+            this.btnSaveURL.Location = new System.Drawing.Point(16, 402);
+            this.btnSaveURL.Name = "btnSaveURL";
+            this.btnSaveURL.Size = new System.Drawing.Size(232, 23);
+            this.btnSaveURL.TabIndex = 0;
+            this.btnSaveURL.Text = "Save blocked URLs";
+            this.btnSaveURL.UseVisualStyleBackColor = true;
+            this.btnSaveURL.Click += new System.EventHandler(this.btnSaveURL_Click);
+            // 
+            // txtURLs
+            // 
+            this.txtURLs.Location = new System.Drawing.Point(16, 52);
+            this.txtURLs.Multiline = true;
+            this.txtURLs.Name = "txtURLs";
+            this.txtURLs.Size = new System.Drawing.Size(232, 344);
+            this.txtURLs.TabIndex = 1;
+            // 
+            // lblURLFormat
+            // 
+            this.lblURLFormat.AutoSize = true;
+            this.lblURLFormat.Location = new System.Drawing.Point(13, 23);
+            this.lblURLFormat.Name = "lblURLFormat";
+            this.lblURLFormat.Size = new System.Drawing.Size(107, 26);
+            this.lblURLFormat.TabIndex = 3;
+            this.lblURLFormat.Text = "Format: example.com\r\n(One per line)";
+            // 
+            // gbOptions
+            // 
+            this.gbOptions.Controls.Add(this.gbKeepAlive);
+            this.gbOptions.Location = new System.Drawing.Point(278, 136);
+            this.gbOptions.Name = "gbOptions";
+            this.gbOptions.Size = new System.Drawing.Size(180, 289);
+            this.gbOptions.TabIndex = 7;
+            this.gbOptions.TabStop = false;
+            this.gbOptions.Text = "Options";
+            // 
+            // gbKeepAlive
+            // 
+            this.gbKeepAlive.Controls.Add(this.btnKeepAliveUninstall);
+            this.gbKeepAlive.Controls.Add(this.btnKeepAliveInstall);
+            this.gbKeepAlive.Controls.Add(this.lblKeepAliveInfo);
+            this.gbKeepAlive.Location = new System.Drawing.Point(6, 164);
+            this.gbKeepAlive.Name = "gbKeepAlive";
+            this.gbKeepAlive.Size = new System.Drawing.Size(168, 119);
+            this.gbKeepAlive.TabIndex = 7;
+            this.gbKeepAlive.TabStop = false;
+            this.gbKeepAlive.Text = "Keep Alive";
+            // 
+            // btnKeepAliveUninstall
+            // 
+            this.btnKeepAliveUninstall.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnKeepAliveUninstall.Enabled = false;
+            this.btnKeepAliveUninstall.Location = new System.Drawing.Point(3, 78);
+            this.btnKeepAliveUninstall.Name = "btnKeepAliveUninstall";
+            this.btnKeepAliveUninstall.Size = new System.Drawing.Size(162, 23);
+            this.btnKeepAliveUninstall.TabIndex = 7;
+            this.btnKeepAliveUninstall.Text = "Uninstall Keep Alive";
+            this.btnKeepAliveUninstall.UseVisualStyleBackColor = true;
+            // 
+            // btnKeepAliveInstall
+            // 
+            this.btnKeepAliveInstall.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnKeepAliveInstall.Location = new System.Drawing.Point(3, 55);
+            this.btnKeepAliveInstall.Name = "btnKeepAliveInstall";
+            this.btnKeepAliveInstall.Size = new System.Drawing.Size(162, 23);
+            this.btnKeepAliveInstall.TabIndex = 6;
+            this.btnKeepAliveInstall.Text = "Install Keep Alive";
+            this.btnKeepAliveInstall.UseVisualStyleBackColor = true;
+            // 
+            // lblKeepAliveInfo
+            // 
+            this.lblKeepAliveInfo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblKeepAliveInfo.Location = new System.Drawing.Point(3, 16);
+            this.lblKeepAliveInfo.Name = "lblKeepAliveInfo";
+            this.lblKeepAliveInfo.Size = new System.Drawing.Size(162, 39);
+            this.lblKeepAliveInfo.TabIndex = 5;
+            this.lblKeepAliveInfo.Text = "Open hosting program if manually closed by user.";
+            // 
+            // statusStripMain
+            // 
+            this.statusStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslblSpacer,
+            this.tsslblWebServer});
+            this.statusStripMain.Location = new System.Drawing.Point(0, 457);
+            this.statusStripMain.Name = "statusStripMain";
+            this.statusStripMain.Size = new System.Drawing.Size(483, 22);
+            this.statusStripMain.TabIndex = 8;
+            this.statusStripMain.Text = "statusStrip1";
+            // 
+            // tsslblWebServer
+            // 
+            this.tsslblWebServer.Name = "tsslblWebServer";
+            this.tsslblWebServer.Size = new System.Drawing.Size(109, 17);
+            this.tsslblWebServer.Text = "Web server: STATUS";
+            // 
+            // tsslblSpacer
+            // 
+            this.tsslblSpacer.Name = "tsslblSpacer";
+            this.tsslblSpacer.Size = new System.Drawing.Size(328, 17);
+            this.tsslblSpacer.Spring = true;
+            // 
+            // frmSoftpushOptions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 479);
+            this.Controls.Add(this.statusStripMain);
+            this.Controls.Add(this.gbOptions);
+            this.Controls.Add(this.lblURLFormat);
+            this.Controls.Add(this.lblBlockedURLs);
+            this.Controls.Add(this.txtURLs);
+            this.Controls.Add(this.btnSaveURL);
+            this.Name = "frmSoftpushOptions";
+            this.Text = "Softpush Options";
+            this.Load += new System.EventHandler(this.frmSoftpushOptions_Load);
+            this.gbOptions.ResumeLayout(false);
+            this.gbKeepAlive.ResumeLayout(false);
+            this.statusStripMain.ResumeLayout(false);
+            this.statusStripMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBlockedURLs;
+        private System.Windows.Forms.Button btnSaveURL;
+        private System.Windows.Forms.TextBox txtURLs;
+        private System.Windows.Forms.Label lblURLFormat;
+        private System.Windows.Forms.GroupBox gbOptions;
+        private System.Windows.Forms.GroupBox gbKeepAlive;
+        private System.Windows.Forms.Button btnKeepAliveUninstall;
+        private System.Windows.Forms.Button btnKeepAliveInstall;
+        private System.Windows.Forms.Label lblKeepAliveInfo;
+        private System.Windows.Forms.StatusStrip statusStripMain;
+        private System.Windows.Forms.ToolStripStatusLabel tsslblWebServer;
+        private System.Windows.Forms.ToolStripStatusLabel tsslblSpacer;
+    }
+}
+
---
+++ Softpush/SettingsWindow.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Globalization;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+// Created by TechNobo
+// Simple program to redirect a website to a locally hosted .html file.
+//
+// How it works:
+// requestedsite.com >> 127.0.4.20 [Non-existent IP address] >> 127.0.0.1:33312 [With hosted html. Can be different port]
+//
+// Uses Windows' hosts file functionality for a DNS redirect.
+// Uses Windows' netsh to redirect from nonexistant IP to your locally hosted file. [hosts can't redirect to localhost, and a custom port. Only works with port 80. Causes conflicts]
+
+
+namespace Softpush
+{
+    public partial class frmSoftpushOptions : Form
+    {
+        TcpClient c;
+        // Basic settings
+        bool pageResponse = true;
+        string pageRedirectURL = "https://google.com";
+        string pageResponsePath = @"Test.txt";
+
+        // Advanced settings.
+        int port = 33312, existStartLine = 0, existEndLine = 0;
+        bool alreadyInHosts = false;
+        string listenaddress = "127.0.4.20";
+        string hostsPath = Environment.GetFolderPath(Environment.SpecialFolder.System) + "\\drivers\\etc\\hosts";
+
+        public frmSoftpushOptions()
+        {
+            InitializeComponent();
+        }
+
+        private string getResponseText()
+        {
+            if (!File.Exists(pageResponsePath))
+            {
+                return "default.html was deleted from the program's directory. Check it exists and is accessible, or redownload it." + Environment.NewLine +
+                    "We will check every minute for a new file -- You can disable this in settings.";
+                // IMPLIMENT TIMED CHECK?
+            }
+            else
+                return File.ReadAllText(pageResponsePath);
+        }
+        private void frmSoftpushOptions_Load(object sender, EventArgs e)
+        {
+            // Add netsh forward for server.
+            // Needed to redirect default URL port (80) to hosted server -- You can't host more than one program on the same port.
+            Debug.WriteLine("Adding netsh v4tov4 redirect (" + listenaddress + " to 127.0.0.1:" + port + ")");
+            string netshCommand =    "netsh interface portproxy add v4tov4 listenport=80 listenaddress=" + listenaddress + " connectport=" + port + " connectaddress=127.0.0.1";
+            string netshCommandSSL = "netsh interface portproxy add v4tov4 listenport=443 listenaddress=" + listenaddress + " connectport=" + port + " connectaddress=127.0.0.1";
+            Debug.WriteLine("-> Executing: "+ netshCommand);
+            Debug.WriteLine("-> Executing: "+ netshCommandSSL);
+            // To check netsh redirects, use "netsh interface portproxy dump"
+            // To remove the above code, you'd use the following command (Written to debug command line, but you can manually put it together)
+            // or use `netsh interface portproxy reset`
+            Debug.WriteLine("--> To remove: netsh interface portproxy delete v4tov4 listenport=80 listenaddress=" + listenaddress);
+            Debug.WriteLine("--> And:       netsh interface portproxy delete v4tov4 listenport=443 listenaddress=" + listenaddress);
+            Debug.WriteLine(netshCommand + " && " + netshCommandSSL + "&& pause");
+            System.Diagnostics.Process.Start("cmd.exe", "/c " + netshCommand + " & " + netshCommandSSL);
+
+            // Load existing settings from hosts file
+            loadExisting();
+
+
+            //Set up and start listener
+            string[] prefixes = new string[1];
+            prefixes[0] = "http://*:" + port + "/";
+
+            Thread thread = new Thread(() => ProcessRequests(prefixes));
+            thread.Start();
+        }
+        private void ProcessRequests(string[] prefixes)
+        {
+            // Tell user server is starting
+            tsslblWebServer.Text = "Web server: Starting...";
+
+            if (!System.Net.HttpListener.IsSupported)
+            {
+                Debug.WriteLine("Windows XP SP2 or higher required.");
+                return;
+            }
+
+            if (prefixes == null || prefixes.Length == 0)
+                throw new ArgumentException("prefixes");
+
+            System.Net.HttpListener listener = new System.Net.HttpListener();
+            foreach (string s in prefixes)
+                listener.Prefixes.Add(s);
+
+            try
+            {
+                listener.Start();
+                Debug.WriteLine("");
+                Debug.WriteLine("Listening...");
+                // Tell user server is starting
+                tsslblWebServer.Text = "Web server: OK";
+                for (int i = 0; i <= 2; i += 0)
+                {
+                    HttpListenerResponse response = null;
+                    try
+                    {
+                        HttpListenerContext context = listener.GetContext();
+                        string inURL = context.Request.RawUrl;
+                        string responseString = "";
+                        response = context.Response;
+                        if (pageResponse)
+                        {
+                            // Send user the html page.
+                            responseString = getResponseText();
+                        }
+                        else
+                        {
+                            // Redirect user to the selected page instead.
+                            responseString = "<html><head><meta http-equiv=\"Refresh\" content=\"5; url = " + pageRedirectURL + "\"></head></html>";
+                        }
+
+                        byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                        response.ContentLength64 = buffer.Length;
+                        System.IO.Stream output = response.OutputStream;
+                        output.Write(buffer, 0, buffer.Length);
+                    }
+                    catch (HttpListenerException ex)
+                    {
+                        Debug.WriteLine(ex.Message);
+                    }
+                    finally
+                    {
+                        if (response != null)
+                            response.Close();
+                    }
+                }
+            }
+            catch (HttpListenerException ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            finally
+            {
+                listener.Close();
+                Debug.WriteLine("Done Listening...");
+            }
+        }
+
+        private void btnSaveURL_Click(object sender, EventArgs e)
+        {
+            // Remove whitespace lines
+            txtURLs.Text = Regex.Replace(txtURLs.Text, @"^\s*$[\r\n]*", string.Empty, RegexOptions.Multiline).TrimEnd();
+
+            // Add IP Address next to URL for hosts file.
+            string[] URLs = txtURLs.Lines;
+            int i = 0;
+            foreach (string url in URLs)
+            {
+                URLs[i] = listenaddress + " " + url;
+                i++;
+            }
+
+            // Check access to hosts file
+            Debug.WriteLine(URLs);                                                                                      // TEMP
+            Debug.WriteLine(hostsPath);                                                                                 // TEMP
+
+
+            // Check if already written rules to hosts file
+            Stream hostsReadStr = new FileStream(hostsPath, FileMode.Open, FileAccess.Read);
+            StreamReader hostsReader = new StreamReader(hostsReadStr);
+            int currentLine = 0;
+
+
+            // Check to see if information is already written to hosts file
+            // See where it starts and ends if it has already bene written.
+            Debug.WriteLine("->Reading hosts file");
+            string line = "";
+            while ((line = hostsReader.ReadLine()) != null)
+            {
+                Debug.WriteLine(line);                                                                                  // TEMP
+                if (line == "# >>>>>>>> Softpush  redirects <<<<<<<<" && existStartLine == 0) // First line is current. First line not already found.
+                    existStartLine = currentLine;
+                else if (existStartLine != 0 && line == "# >>> End of Softpush redirects <<<") // First line found. Last line is current.
+                {
+                    existEndLine = currentLine;
+                    break;
+                }
+                currentLine++;
+            }
+            hostsReader.Close();
+            currentLine = 0;
+
+
+            // Copy the hosts file redirects and information that is NOT added by this program.
+            // If info from this program is written, it skips it.
+            string tempFile = Path.GetTempFileName();
+            Debug.WriteLine("Temp hosts file: " + tempFile);
+            Debug.WriteLine("Starting copy from hosts to temporary hosts file.");
+            using (var sr = new StreamReader(hostsPath))
+            using (var sw = new StreamWriter(tempFile))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if ((existStartLine != 0 && !(currentLine >= existStartLine && currentLine <= existEndLine)) || existStartLine == 0) // Copies if: (Redirects written AND not in upper/lower bounds) OR (Redirects not writen)
+                    {
+                        sw.WriteLine(line);
+                    }
+                    currentLine++;
+                }
+                Debug.WriteLine("Finished copying hosts text that was NOT put in by this program");
+                Debug.WriteLine("Adding redirects and information.");
+                sw.WriteLine("# >>>>>>>> Softpush  redirects <<<<<<<<");
+                sw.WriteLine("# >> Created by TechNobo");
+                sw.WriteLine("# >> github link when it exists.");
+                sw.WriteLine("# -------------------------------------");
+                sw.WriteLine("# >> DO NOT EDIT - USE THE INTERFACE <<");
+
+                foreach (string URL in URLs)
+                    sw.WriteLine(URL);
+
+                sw.WriteLine("# >>> End of Softpush redirects <<<");
+            }
+
+            // Copy information from temp hosts file to Windows' hosts file, without deleting either. [Will give "in use by another program" if attempted without doing it this long way around].
+            Debug.WriteLine("Emptying the hosts file.");
+            File.WriteAllText(hostsPath, string.Empty);
+
+            Debug.WriteLine("Copying temporary hosts file to replace original hosts file.");
+            using (var sr = new StreamReader(tempFile))
+            using (var sw = new StreamWriter(hostsPath))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    sw.WriteLine(line);
+                }
+            }
+            Debug.WriteLine("Done!");
+            File.Delete(tempFile);
+        }
+
+        private void loadExisting()
+        {
+            using (var sr = new StreamReader(hostsPath))
+            {
+                Debug.WriteLine("->Reading hosts file");
+                int currentLine = 0;
+                string line = "";
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Debug.WriteLine(line);                                                                                  // TEMP
+                    if (line == "# >>>>>>>> Softpush  redirects <<<<<<<<" && existStartLine == 0) // First line is current. First line not already found.
+                        existStartLine = currentLine;
+                    else if (existStartLine != 0 && line == "# >>> End of Softpush redirects <<<") // First line found. Last line is current.
+                    {
+                        existEndLine = currentLine;
+                        alreadyInHosts = true;
+                        break;
+                    } else if (existStartLine != 0 && !line.StartsWith("#")) // Start found. Not a commented line. This is where the IPs are, as long as it's not broken.
+                    {
+                        txtURLs.AppendText(line.Split(' ')[1]+"\r\n");
+                    }
+                    currentLine++;
+                }
+
+            }
+        }
+    }
+}
+
+// hosts file overlay
+//
+// ---   Windows info   ---
+// --- User information ---
+//
+// # >>>>>>>> Softpush  redirects <<<<<<<<
+// # >> Created by TechNobo
+// # >> github link when it exists.
+// # -------------------------------------
+// # >> DO NOT EDIT - USE THE INTERFACE <<
+// 127.0.4.20 example.com
+// 127.0.4.20 example2.com
+// # >>> End of Softpush redirects <<<
+//
